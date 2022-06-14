7c612bbeb5ef1b112ed04c1927b7fbb63f47a510
RostenRoss <costantini.nestor.m@gmail.com>
2022-06-14 00:38:46 -0300

    Se agrega la funcionalidad de que al posar un rato el mouse sobre una figura esta despliega un pequeño menu, el menu esta sin funcionalidades.

--- TPI_Diagramador/DiagramImg.cs
+++ TPI_Diagramador/DiagramImg.cs
@@ -14,6 +14,8 @@ namespace TPI_Diagramador
         private string nombreFigura;
         private string colorFigura;
         private string textoImagen;
+        private Panel menu;
+        private bool isButonsFocus;
 
         public string TextoImagen { get { return this.textoImagen; } set { this.textoImagen = value; } }
         public string NombreFigura { get { return this.nombreFigura; } set { this.nombreFigura = value; } }
@@ -24,29 +26,49 @@ namespace TPI_Diagramador
         {
             focus = false;
             colorRecuadro = Color.Coral;
-
+            menu= new Panel();
+            menu.Name = "menu";
         }
 
         public override string ToString()
         {
             return "color: " + this.colorFigura + ", figura: " + nombreFigura + ", focus: " + this.focus;
         }
-        public void setFocusTrue()
-        {
-            this.focus = true;
-        }
-        public void setFocusFalse()
-        {
-            this.focus = false;
-        }
+        
         //Cuando el mouse dekja la superficie del elemento
         protected override void OnMouseLeave(EventArgs e)
         {
-            System.Diagnostics.Debug.WriteLine("OnMouseLeave" + this.focus + ", " + this.Name);
+            System.Diagnostics.Debug.WriteLine(" OnMouseLeave menu focus : " + isButonsFocus);
+
+            if (isButonsFocus)
+            {
+                this.Parent.Controls.RemoveByKey("menu");
+                this.menu.Dispose();
+                this.menu = new Panel();
+                menu.Name = "menu";
+            }
+
             this.Refresh();
         }
-
        
+        private void onMouseHoverButonvoid (object sender, System.EventArgs e)
+        {
+            this.isButonsFocus = true;
+            System.Diagnostics.Debug.WriteLine(" onMouseHoverButonvoid menu focus : " + isButonsFocus);
+
+        }
+
+        private void onMouseLeaveButonvoid(object sender, System.EventArgs e)
+        {
+            this.isButonsFocus = false;
+            System.Diagnostics.Debug.WriteLine("onMouseLeaveButonvoid fuera del boton");
+            this.Parent.Controls.RemoveByKey("menu");
+            this.menu.Dispose();
+            this.menu = new Panel();
+            menu.Name = "menu";
+
+        }
+
         public void dibujarContorno() 
         {
             Graphics g = Graphics.FromImage(this.Image);
@@ -60,7 +82,14 @@ namespace TPI_Diagramador
             Rectangle r = Rectangle.FromLTRB(0, 0, this.Width, this.Height);
             ControlPaint.DrawBorder(g, r, color, ButtonBorderStyle.Solid);
         }
+        protected override void OnMouseHover(EventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine("mouse dentro");
+            crearMenu();
+            System.Diagnostics.Debug.WriteLine("menu focus : " + isButonsFocus);
 
+            //base.OnMouseHover(e);
+        }
         //Cuando el mouse entra en el area del elemento
         protected override void OnMouseEnter(EventArgs e)
         {
@@ -85,14 +114,25 @@ namespace TPI_Diagramador
             {
                 this.Left += e.X - point.X;
                 this.Top += e.Y - point.Y;
+                System.Diagnostics.Debug.WriteLine("mouse fuera del boton");
+
+                this.Parent.Controls.Remove(menu);
+                this.menu.Dispose();
+                this.menu = new Panel();
             }
+
         }               
 
         public void moverFigura(MouseEventArgs e)
         {
             int X = e.X;
             int Y = e.Y;
+
             int delta = e.Delta;
+            this.Parent.Controls.RemoveByKey("menu");
+            this.menu.Dispose();
+            this.menu = new Panel();
+            menu.Name = "menu";
 
             MouseEventArgs evento = new MouseEventArgs(e.Button,e.Clicks, X, Y, delta);
             OnMouseMove(evento);
@@ -117,6 +157,111 @@ namespace TPI_Diagramador
             graphics.DrawString(t, font, Brushes.Black, new Point(0, 0));            
             this.Image = image;
 
+        }       
+
+        public void crearMenu()
+        {
+            this.menu = new Panel();
+            menu.Name = "menu";
+            menu.AutoSize = true;
+            menu.BackColor = Color.LawnGreen;
+            menu.MouseEnter += new System.EventHandler(onMouseHoverButonvoid);
+            menu.MouseLeave += new System.EventHandler(onMouseLeaveButonvoid);
+  
+
+            Button botonAgrandar = new Button();
+            botonAgrandar.Text = "+";
+            botonAgrandar.Height = 20;
+            botonAgrandar.Width = 20;
+            botonAgrandar.Dock= DockStyle.Right;
+            //botonAgrandar.MouseLeave += new System.EventHandler(onMouseLeaveButonvoid);
+            //botonAgrandar.Location = new Point(this.Location.X + (this.Width / 3), this.Location.Y - 20);            
+            menu.Width = botonAgrandar.Width;
+            menu.Height = botonAgrandar.Height;
+            menu.Controls.Add(botonAgrandar);
+
+
+            Button botonAchicar = new Button();
+            botonAchicar.Text = "-";
+            botonAchicar.Height = 20;
+            botonAchicar.Width = 20;
+            botonAchicar.Dock = DockStyle.Right;
+           // botonAchicar.MouseLeave += new System.EventHandler(onMouseLeaveButonvoid);
+
+            menu.Width += botonAchicar.Width;
+            menu.Controls.Add(botonAchicar);
+
+            Button colorRojo = new Button();
+            colorRojo.BackColor = Color.Red;
+            colorRojo.Height = 20;
+            colorRojo.Width = 20;
+            colorRojo.Dock = DockStyle.Right;
+           // colorRojo.MouseLeave += new System.EventHandler(onMouseLeaveButonvoid);
+
+            menu.Width += colorRojo.Width;
+            menu.Controls.Add(colorRojo);
+
+            Button colorNegro = new Button();
+            colorNegro.BackColor = Color.Black;
+            colorNegro.Height = 20;
+            colorNegro.Width = 20;
+            colorNegro.Dock = DockStyle.Right;
+           // colorNegro.MouseLeave += new System.EventHandler(onMouseLeaveButonvoid);
+
+            menu.Width += colorNegro.Width;
+            menu.Controls.Add(colorNegro);
+
+            Button colorAmarillo = new Button();
+            colorAmarillo.BackColor = Color.Yellow;
+            colorAmarillo.Height = 20;
+            colorAmarillo.Width = 20;
+            colorAmarillo.Dock = DockStyle.Right;
+           // colorAmarillo.MouseLeave += new System.EventHandler(onMouseLeaveButonvoid);
+
+            menu.Width += colorAmarillo.Width;
+            menu.Controls.Add(colorAmarillo);
+
+            Button colorVerde = new Button();
+            colorVerde.BackColor = Color.Green;
+            colorVerde.Height = 20;
+            colorVerde.Width = 20;
+            colorVerde.Dock = DockStyle.Right;
+            //colorVerde.MouseLeave += new System.EventHandler(onMouseLeaveButonvoid);
+
+            menu.Width += colorVerde.Width;
+            menu.Controls.Add(colorVerde);
+
+            Button colorCeleste = new Button();
+            colorCeleste.BackColor = Color.LightBlue;
+            colorCeleste.Height = 20;
+            colorCeleste.Width = 20;
+            colorCeleste.Dock = DockStyle.Right;
+           // colorCeleste.MouseLeave += new System.EventHandler(onMouseLeaveButonvoid);
+
+            menu.Width += colorCeleste.Width;
+            menu.Controls.Add(colorCeleste);
+
+            Button colorNaranja = new Button();
+            colorNaranja.BackColor = Color.Orange;
+            colorNaranja.Height = 20;
+            colorNaranja.Width = 20;
+            colorNaranja.Dock = DockStyle.Right;
+          // colorNaranja.MouseLeave += new System.EventHandler(onMouseLeaveButonvoid);
+
+            menu.Width += colorNaranja.Width;
+            menu.Controls.Add(colorNaranja);
+
+            Button colorMorado = new Button();
+            colorMorado.BackColor = Color.RebeccaPurple;
+            colorMorado.Height = 20;
+            colorMorado.Width = 20;
+            colorMorado.Dock = DockStyle.Right;
+          //  colorMorado.MouseLeave += new System.EventHandler(onMouseLeaveButonvoid);
+
+            menu.Width += colorMorado.Width;
+            menu.Controls.Add(colorMorado);
+            menu.Location = new Point( (this.Location.X-(menu.Width/3)), this.Location.Y - 20);
+            this.Parent.Controls.Add(menu);
         }       
     }
 }
